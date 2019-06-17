07d721969882895fdfb85f04a391889974da1c0c
Irsan Arisandy <irsanarisandy@hotmail.com>
2019-06-17 14:06:10 +1200

    Add fetch data functionality

--- pages/Post.jsx
+++ pages/Post.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import { withRouter } from 'next/router';
+import axios from 'axios';
 import Layout from '../components/Layout';
 
-const Page = withRouter(props => (
+const Post = props => (
   <Layout>
-    <h1>{props.router.query.title}</h1>
-    <p>This is the blog post content.</p>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.show.image.medium} />
   </Layout>
-));
+);
 
-export default Page;
+Post.getInitialProps = async(context) => {
+  const { id } = context.query;
+  const show = await axios.get(`https://api.tvmaze.com/shows/${id}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      // Error (copied catch error from https://gist.github.com/fgilio/230ccd514e9381fafa51608fcf137253)
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
+};
+
+export default Post;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,24 +1,53 @@
 import React from 'react';
 import Link from 'next/link';
+import axios from 'axios';
 import Layout from '../components/Layout';
 
-const PostLink = props => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
-);
-
-const Blog = () => (
+const Index = (props) => (
   <Layout>
-    <h1>My Blog</h1>
+    <h1>Batman TV Shows</h1>
     <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js" />
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
   </Layout>
 );
 
-export default Blog;
+Index.getInitialProps = async() => {
+  let data = await axios.get('https://api.tvmaze.com/search/shows?q=batman')
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      // Error (copied catch error from https://gist.github.com/fgilio/230ccd514e9381fafa51608fcf137253)
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(entry => entry.show)
+  };
+};
+
+export default Index;
